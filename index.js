788a21648daadd00e36cf877dca5da36cae1016b
Mazahir Mehdi <syedmazahir099@gmail.com>
2026-02-25 23:54:35 +0500

    fix: commits to understand by devs

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require('express');     
 const app = express();
 const path = require('path');
 const fs = require('fs');
 
-app.use(express.json());
+//Parsers Setting
+app.use(express.json());                        
 app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res) =>{
     fs.readdir(`./files`, (err,files)=>{
-        res.render("index", {files:files});
+        res.render("index", {files:files});     //rendring data to index ejs file
     })
 })
-app.get('/show/:filename',(req,res) =>{
+app.get('/show/:filename',(req,res) =>{     //filename as variable to store the different names
     fs.readFile(`./files/${req.params.filename}`,"utf-8", (err,filedata)=>{
         res.render("show", {filename:req.params.filename, filedata:filedata});
     });
@@ -22,7 +23,7 @@ app.get('/edit/:filename',(req,res) =>{
     res.render("edit", {filename:req.params.filename});
 })
 
-app.post('/create',(req,res)=>{
+app.post('/create',(req,res)=>{     //naming the filetitle
     fs.writeFile(`./files/${req.body.title.split(" ").join('')}.txt`, req.body.title, (err)=>{
         res.redirect("/");
     });
